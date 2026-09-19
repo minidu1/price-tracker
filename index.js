cbb1bf0e0561194c68827947208885a1f4bfcbcd
minidu1 <lmmdsilva@students.nsbm.ac.lk>
2026-09-19 23:06:48 +0530

    init prisma

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json()); // lets Express understand JSON sent in requests
+let products = []; // temporary in-memory "database"
+
+
 app.get('/', (req, res) => {
   res.send('Server is running');
+});
+
+app.post('/products', (req, res) => {
+  const { name, url } = req.body;
+  const newProduct = { id: products.length + 1, name, url, price: null };
+  products.push(newProduct);
+  res.status(201).json(newProduct);
+});
+
+app.get('/products', (req, res) => {
+  res.json(products);
 });
 
 app.listen(PORT, () => {
