c59f78747937ee75548d81e232baf2af6c7288a8
minidu1 <lmmdsilva@students.nsbm.ac.lk>
2026-09-23 13:52:12 +0530

    feat: update price scraping logic and enhance error handling - save price history to db - compare price with target price

--- index.js
+++ index.js
@@ -16,9 +16,8 @@ const PORT = 3000;
 
 async function scrapePrice(url) {
   const browser = await puppeteer.launch({
-    executablePath:
-      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-  });
+    executablePath: process.env.CHROME_PATH
+    });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "domcontentloaded" });
   await page.waitForSelector(".pdp-price_type_normal", { timeout: 10000 });
@@ -26,30 +25,60 @@ async function scrapePrice(url) {
     ".pdp-price_type_normal",
     (el) => el.textContent,
   );
-  await browser.close; //close the browser
+  await browser.close(); //close the browser
   const numericPrice = parseFloat(priceText.match(/\d+(?:\.\d+)?/)?.[0]);
   return numericPrice;
 }
 
+async function savePriceHistory(productId, price) {
+  await prisma.priceHistory.create({
+    data: {
+      productId,
+      date: new Date(),
+      price,
+    },
+  });
+}
+
+async function comparePriceWithTarget(productId, price) {
+  const targetPrices = await prisma.trackedProduct.findMany({
+    where: { productId, notified: false },
+    select: { target: true, uid: true },
+  });
+
+  for (const targetPrice of targetPrices) {
+    if (price <= targetPrice.target){
+      console.log("price is",price, "target is", targetPrice.target);
+      
+    }
+    else{
+      console.log(`price is ${price} but target is ${targetPrice.target}`);
+      
+    }
+  }
+}
+
 async function checkAllProducts() {
-  const productLinks = await prisma.product.findMany({
+  const products = await prisma.product.findMany({
     select: {
       link: true,
+      id: true,
     },
   });
 
-  for (const product of productLinks) {
+  for (const product of products) {
     try {
       const price = await scrapePrice(product.link);
-      console.log(price);
+      // await savePriceHistory(product.id, price);
+      await comparePriceWithTarget(product.id, price);
     } catch (err) {
-      console.log("Failed to fetch the price:", err.message);
+      console.log(`Failed processing ${product.link}:`, err.message);
     }
   }
 
   return;
 }
-const products = await checkAllProducts();
+await checkAllProducts();
 
 // endpoint calls
 app.use(express.json()); // lets Express understand JSON sent in requests
@@ -59,6 +88,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/products", async (req, res) => {
+  // *** need to validate parsing data *** //
   const { uid, link, name, target } = req.body;
 
   const newProduct = await prisma.trackedProduct.create({
